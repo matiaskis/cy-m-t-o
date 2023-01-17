3adb2633a06370d10aca99c81fc40bc7e1b317a3
khorz <104204070+khorz@users.noreply.github.com>
2023-01-17 11:50:54 +0100

    Create avl.h

---
+++ avl.h
@@ -0,0 +1,38 @@
+#ifndef AVL_H
+#define AVL_H
+
+typedef struct data{
+    int station;
+    float associated_data;
+    int average_increment;
+    int doublon;
+}Data;
+
+typedef struct avl{
+    Data elmt;
+    struct avl * fg;
+    struct avl * fd;
+    int equilibre;
+}AVL;
+
+
+typedef AVL* PAVL;
+
+void traiter(Data e,FILE* out);
+void parcoursInfixe(PAVL a, FILE* out);
+void parcoursInfixeR(PAVL a, FILE* out);
+int max(int a, int b);
+int min(int a, int b);
+PAVL creerArbre(Data e);
+
+PAVL rotationGauche(PAVL a);
+PAVL rotationDroite(PAVL a);
+PAVL doublerotationGauche(PAVL a);
+PAVL doublerotationDroite(PAVL a);
+PAVL equilibrerAVL(PAVL a);
+PAVL insertionAVLMax(PAVL a,Data e, int* h);
+PAVL insertionAVLMin(PAVL a,Data e, int* h);
+PAVL insertionAVLAverage(PAVL a,Data e, int* h);
+void calculateAverage(PAVL a);
+PAVL insertionAVLHeight(PAVL a,Data e, int* h);
+#endif
