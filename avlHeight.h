deb50e89b149a544f00b685f56ae46d0679f64c7
matiaskis <104195775+matiaskis@users.noreply.github.com>
2023-01-19 15:12:56 +0100

    Update avlHeight.h

--- avlHeight.h
+++ avlHeight.h
@@ -1,1 +1,34 @@
+#ifndef AVL_H
+#define AVL_H
 
+typedef struct data{
+    int station;
+    float associated_data;
+    int average_increment;
+
+}Data;
+
+typedef struct avl{
+    Data elmt;
+    struct avl * fg;
+    struct avl * fd;
+    int equilibre;
+}AVL;
+
+
+typedef AVL* PAVL;
+
+void traiter(Data e,FILE* out);
+void parcoursInfixe(PAVL a, FILE* out);
+void parcoursInfixeR(PAVL a, FILE* out);
+int max(int a, int b);
+int min(int a, int b);
+PAVL creerArbre(Data e);
+
+PAVL rotationGauche(PAVL a);
+PAVL rotationDroite(PAVL a);
+PAVL doublerotationGauche(PAVL a);
+PAVL doublerotationDroite(PAVL a);
+PAVL equilibrerAVL(PAVL a);
+PAVL insertionAVLHeight(PAVL a,Data e, int* h)
+#endif
