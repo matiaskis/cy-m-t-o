915703bb0c48759f1652a14d476bfd16cc483a98
matiaskis <104195775+matiaskis@users.noreply.github.com>
2023-01-31 18:45:08 +0100

    Update abrm.h

--- abrm.h
+++ abrm.h
@@ -20,16 +20,75 @@ typedef struct abr_m{
 
 typedef ABR_m* PABR_m;
 
-void treat_abrm(int count,Data_abrm e,FILE* out);
-void treat_abrm2(Data_abrm e,FILE* out);
-void infixPath_abrm(PABR_m a, FILE* out);
-void infixPathR_abrm(PABR_m a, FILE* out);
-void infixPath_abrm2(PABR_m a, FILE* out);
-void infixPathR_abrm2(PABR_m a, FILE* out);
+    /*!
+     * @brief                       this function is used to write all values needed in the output file                                            
+     * @param  e                    current element written
+     * @param  out                  pointer relative to ouput file
+     * @return                      nothing
+     */
+    void treat_abrm(int count,Data_abrm e,FILE* out);
 
-PABR_m creerArbre_abrm(Data_abrm e);
+    /*!
+     * @brief                       this function is used to write all values needed in the output file                                            
+     * @param  e                    current element written
+     * @param  out                  pointer relative to ouput file
+     * @return                      nothing
+     */
+    void treat_abrm2(Data_abrm e,FILE* out);
 
-PABR_m insertionABRM(PABR_m a,Data_abrm e);
-PABR_m insertionABRMMax(PABR_m a,Data_abrm e);
+/*!
+     * @brief                       this function is used to browse into  the tree                                            
+     * @param  a                    pointer relative to the tree
+     * @param  out                  pointer relative to ouput file
+     * @return                      nothing
+     */
+    void infixPath_abrm(PABR_m a, FILE* out);
+
+/*!
+     * @brief                       this function is used to browse into  the tree but in reverse                                           
+     * @param  a                    pointer relative to the tree
+     * @param  out                  pointer relative to ouput file
+     * @return                      nothing
+     */
+    void infixPathR_abrm(PABR_m a, FILE* out);
+
+/*!
+     * @brief                       this function is used to browse into  the tree                                            
+     * @param  a                    pointer relative to the tree
+     * @param  out                  pointer relative to ouput file
+     * @return                      nothing
+     */
+    void infixPath_abrm2(PABR_m a, FILE* out);
+
+/*!
+     * @brief                       this function is used to browse into  the tree but in reverse                                           
+     * @param  a                    pointer relative to the tree
+     * @param  out                  pointer relative to ouput file
+     * @return                      nothing
+     */
+    void infixPathR_abrm2(PABR_m a, FILE* out);
+
+/*!
+     * @brief                       this function is used to create a new tree                                         
+     * @param  e                    element you want to insert first in the tree
+     * @return                      pointer relative to the tree
+     */
+    PABR_m creerArbre_abrm(Data_abrm e);
+
+/*!
+     * @brief                       this function is used to insert an element in the tree                                        
+     * @param  a                    pointer relative to the tree
+     * @param  e                    element to insert
+     * @return                      pointer relative to the tree
+     */
+    PABR_m insertionABRM(PABR_m a,Data_abrm e);
+
+/*!
+     * @brief                       this function is used to insert an element in the tree, keeping the max value of associated_data                                         
+     * @param  a                    pointer relative to the tree
+     * @param  e                    element to insert
+     * @return                      pointer relative to the tree
+     */
+    PABR_m insertionABRMMax(PABR_m a,Data_abrm e);
 
 #endif
