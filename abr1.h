1d841db1a84e8850f49ad65d7120c06319c5b438
matiaskis <104195775+matiaskis@users.noreply.github.com>
2023-01-31 13:52:14 +0100

    Update abr1.h

--- abr1.h
+++ abr1.h
@@ -12,18 +12,17 @@ typedef struct data1ABR{
 }Data1ABR;
 
 typedef struct abr1{
-    Data elmt;
+    Data1ABR elmt;
     struct abr1 * fg;
     struct abr1 * fd;
-    int equilibre;
 }ABR1;
 
 typedef ABR1* PABR1;
 
-void traiter_abr1(Data1ABR e,FILE* out) ;
-void parcoursInfixe_abr1(PABR1 a, FILE* out);
-void parcoursInfixeR_abr1(PABR1 a, FILE* out);
-PABR1 creerArbre_abr1(Data1ABR e);
+void treat_abr1(Data1ABR e,FILE* out) ;
+void infixPath_abr1(PABR1 a, FILE* out);
+void infixPathR_abr1(PABR1 a, FILE* out);
+PABR1 createTree_abr1(Data1ABR e);
 PABR1 insertionMax_abr1(PABR1 a,Data1ABR e);
 PABR1 insertionMin_abr1(PABR1 a,Data1ABR e);
 PABR1 insertionAverage_abr1(PABR1 a, Data1ABR e);
