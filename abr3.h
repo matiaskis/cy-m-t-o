017a0d070f936a5e0764033d251b139a99ff0f3a
matiaskis <104195775+matiaskis@users.noreply.github.com>
2023-01-31 17:37:03 +0100

    Update abr3.h

--- abr3.h
+++ abr3.h
@@ -20,13 +20,73 @@ typedef struct abr3{
 typedef ABR3* PABR3;
 
 
-void treat_abr3(Data_abr3 e,FILE* out);
-void secondInfixPath(PABR3 a,FILE * out);
-void infixPath_abr3(PABR3 a, FILE* out) ;
-void secondinfixPathR_abr3(PABR3 a,FILE * out);
-void infixPathR_abr3(PABR3 a, FILE* out) ;
-PABR3 createTree_abr3(Data_abr3 e);
-PABR3 createTree_oabr3(Data_abr3 e);
-PABR3 insertionABR3SecondTree(PABR3 a,Data3 e);
-PABR3 insertionABR_oabr3(PABR3 a,Data3 e);
+    /*!
+     * @brief                       this function is used to write all values needed in the output file                                            
+     * @param  e                    current element written
+     * @param  out                  pointer relative to ouput file
+     * @return                      nothing
+     */
+    void treat_abr3(Data_abr3 e,FILE* out);
+
+    /*!
+     * @brief                       this function is used to browse into the second tree used to sort all the associated data                                       
+     * @param  a                    pointer relative to the tree
+     * @param  out                  pointer relative to ouput file
+     * @return                      nothing
+     */
+    void secondInfixPath(PABR3 a,FILE * out);
+
+    /*!
+     * @brief                       this function is used to browse into the tree                                        
+     * @param  a                    pointer relative to the tree
+     * @param  out                  pointer relative to ouput file
+     * @return                      nothing
+     */
+    void infixPath_abr3(PABR3 a, FILE* out) ;
+    
+    /*!
+     * @brief                       this function is used to browse into the second tree used to sort all the associated data but in reverse                                      
+     * @param  a                    pointer relative to the tree
+     * @param  out                  pointer relative to ouput file
+     * @return                      nothing
+     */
+    void secondinfixPathR_abr3(PABR3 a,FILE * out);
+    
+    /*!
+     * @brief                       this function is used to browse into the tree in reverse                                       
+     * @param  a                    pointer relative to the tree
+     * @param  out                  pointer relative to ouput file
+     * @return                      nothing
+     */
+    void infixPathR_abr3(PABR3 a, FILE* out) ;
+    
+     /*!
+     * @brief                       this function is used to create the root of each second tree                                       
+     * @param  e                    element to insert
+     * @return                      pointer relative to the tree
+     */
+    PABR3 createTree_abr3(Data_abr3 e);
+    
+    /*!
+     * @brief                       this function is used to create a new tree                                       
+     * @param  e                    element to insert
+     * @return                      pointer relative to the tree
+     */
+    PABR3 createTree_oabr3(Data_abr3 e);
+    
+    /*!
+     * @brief                       this function is used to insert an element in the second tree                                      
+     * @param  a                    pointer relative to the tree
+     * @param  e                    element to insert
+     * @return                      pointer relative to the tree
+     */
+    PABR3 insertionABR3SecondTree(PABR3 a,Data3 e);
+    
+    /*!
+     * @brief                       this function is used to insert an element in the first tree                                      
+     * @param  a                    pointer relative to the tree
+     * @param  e                    element to insert
+     * @return                      pointer relative to the tree
+     */
+    PABR3 insertionABR_oabr3(PABR3 a,Data3 e);
 #endif
