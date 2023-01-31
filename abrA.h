32febb47107316e04516dd07caed59751a8c050b
matiaskis <104195775+matiaskis@users.noreply.github.com>
2023-01-31 21:35:18 +0100

    Create abrA.h

---
+++ abrA.h
@@ -0,0 +1,67 @@
+#ifndef ABRA_H
+#define ABRA_H
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef struct data_abra{
+    int station;
+    float associated_DataA1;
+    float associated_DataA2;
+    int average_increment;
+}Data_abra;
+
+typedef struct ABRAA{
+    Data_abra elmt;
+    struct ABRAA * fg;
+    struct ABRAA * fd;
+}ABRAA;
+
+typedef ABRAA* PABRAA;
+
+/*!
+     * @brief                       this function is used to write all values needed in the output file                                            
+     * @param  e                    current element written
+     * @param  out                  pointer relative to ouput file
+     * @return                      nothing
+     */
+void treatA(DataA1 e,FILE* out);
+
+/*!
+     * @brief                       this function is used to browse into  the tree                                            
+     * @param  a                    pointer relative to the tree
+     * @param  out                  pointer relative to ouput file
+     * @return                      nothing
+     */
+void infixPathA(PABRAA a, FILE* out);
+
+/*!
+     * @brief                       this function is used to browse into  the tree but in reverse                                           
+     * @param  a                    pointer relative to the tree
+     * @param  out                  pointer relative to ouput file
+     * @return                      nothing
+     */
+void infixPathRA(PABRAA a, FILE* out);
+
+/*!
+     * @brief                       this function is used to create a new tree                                         
+     * @param  e                    element you want to insert first in the tree
+     * @return                      pointer relative to the tree
+     */
+PABRAA createTreeA(Data_abra e);
+
+ /*!
+     * @brief                       this function is used to insert an element in the tree, keeping the the gross value of all associated_data                                          
+     * @param  a                    pointer relative to the tree
+     * @param  e                    element to insert
+     * @return                      pointer relative to the tree
+     */
+PABRAA insertionAVLAAAverage(PABRAA a,Data_abra e, int* h);
+
+/*!
+     * @brief                       this function is used to calculate the average of the associated data in each element of the tree                                         
+     * @param  a                    pointer relative to the tree
+     * @return                      nothing
+     */
+void calculateAverageA(PABRAA a);
+#endif
