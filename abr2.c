91547b35b0bfa0d6f03d47a700d5451d0154c8de
matiaskis <104195775+matiaskis@users.noreply.github.com>
2023-01-31 13:46:50 +0100

    Update abr2.c

--- abr2.c
+++ abr2.c
@@ -1,14 +1,14 @@
 #include "abr2.h"
 
 
-void traiter_abr2(Data_abr2 e,FILE* out) {
+void treat_abr2(Data_abr2 e,FILE* out) {
     long h,d,m,y;
     h=((e.date2%100));
     d=((e.date2%10000)-e.date2%100)/100;
     m=((e.date2%1000000)-e.date2%10000)/10000;
     y=((e.date2)-e.date2%1000000)/1000000;
     fprintf(out,"%ld/%ld/%ld %ld:00:00 ",d,m,y,h);
-    fprintf(out,"%f\n",e.associated_Data2);
+    fprintf(out,"%f\n",e.associated_data);
 }
 
 void parcoursInfixe_abr2(PABR2 a, FILE* out) {
