2ba191a57f29b912f805e5f391c832c6c867efb1
matiaskis <104195775+matiaskis@users.noreply.github.com>
2023-01-31 18:41:47 +0100

    Update abrm.c

--- abrm.c
+++ abrm.c
@@ -2,7 +2,7 @@
 
 
 
-void treat_m(int count,Data_abrm e,FILE* out) {
+void treat_abrm(int count,Data_abrm e,FILE* out) {
     fprintf(out,"%d ",e.station);
     fprintf(out,"%d\n",e.moisture);
    
@@ -15,52 +15,52 @@ void treat_m(int count,Data_abrm e,FILE* out) {
     
   
 }
-void treat_m2(Data_abrm e,FILE* out) {
+void treat_abrm2(Data_abrm e,FILE* out) {
     fprintf(out,"%d ",e.station);
     fprintf(out,"%d\n",e.moisture);
 }
  
 
-void infixPath_m(PABR_m a, FILE* out) {
+void infixPath_abrm(PABR_m a, FILE* out) {
 if (a!=NULL) {
-    infixPath_m(a->fg,out);
-    treat_m(a->elmt.counter,a->elmt,out);
+    infixPath_abrm(a->fg,out);
+    treat_abrm(a->elmt.counter,a->elmt,out);
     free(a);
-    infixPath_m(a->fd,out);
+    infixPath_abrm(a->fd,out);
 }
 }
 
-void infixPathR_m(PABR_m a, FILE* out) {
+void infixPathR_abrm(PABR_m a, FILE* out) {
 if (a!=NULL) {
-    infixPathR_m(a->fd,out); 
-    treat_m(a->elmt.counter,a->elmt,out);
+    infixPathR_abrm(a->fd,out); 
+    treat_abrm(a->elmt.counter,a->elmt,out);
     free(a);
-    infixPathR_m(a->fg,out);
+    infixPathR_abrm(a->fg,out);
 }
 }
 
-void infixPath_m2(PABR_m a, FILE* out) {
+void infixPath_abrm2(PABR_m a, FILE* out) {
 if (a!=NULL) {
-    infixPath_m2(a->fg,out);
-    treat_m2(a->elmt,out);
+    infixPath_abrm2(a->fg,out);
+    treat_abrm2(a->elmt,out);
     free(a);
-    infixPath_m2(a->fd,out);
+    infixPath_abrm2(a->fd,out);
 }
 }
 
-void infixPathR_m2(PABR_m a, FILE* out) {
+void infixPathR_abrm2(PABR_m a, FILE* out) {
 if (a!=NULL) {
-    infixPathR_m2(a->fd,out); 
-    treat_m2(a->elmt,out);
+    infixPathR_abrm2(a->fd,out); 
+    treat_abrm2(a->elmt,out);
     free(a);
-    infixPathR_m2(a->fg,out);
+    infixPathR_abrm2(a->fg,out);
 }
 }
 
 
 
 
-PABR_m createTree_m(Data_abrm e){
+PABR_m createTree_abrm(Data_abrm e){
     PABR_m tree ;
     tree=malloc(sizeof(ABR_m));
     if(tree==NULL){
@@ -79,7 +79,7 @@ PABR_m createTree_m(Data_abrm e){
 PABR_m insertionABRMMax(PABR_m a,Data_abrm e){
 
     if (a== NULL){
-        return createTree_m(e);
+        return createTree_abrm(e);
     }
     else if (e.station < a->elmt.station){
     a->fg=insertionABRMMax(a->fg, e);
@@ -102,7 +102,7 @@ PABR_m insertionABRM(PABR_m a,Data_abrm e){
 
     if (a== NULL){
        
-        return createTree_m(e);
+        return createTree_abrm(e);
     }
     else if (e.moisture < a->elmt.moisture ){
     a->fg=insertionABRM(a->fg, e);
