f824a70f55865be9eafbfbdfa381e510bac52073
matiaskis <104195775+matiaskis@users.noreply.github.com>
2023-01-31 21:21:35 +0100

    Update avlA.c

--- avlA.c
+++ avlA.c
@@ -1,101 +1,101 @@
 #include "avl1.h"
 #include "avlA.h"
 
-void traiterA(DataA1 e,FILE* out) {
+void treatA(DataA1 e,FILE* out) {
     fprintf(out,"%d ",e.station);
     fprintf(out,"%f\n",e.associated_DataA1);
     fprintf(out,"%f\n",e.associated_DataA2);
 }
 
-void parcoursInfixeA(PAVLAA a, FILE* out) {
+void infixePathA(PAVLAA a, FILE* out) {
 if (a!=NULL) {
-    parcoursInfixeA(a->fg,out);
-    traiterA(a->elmt,out);
+    infixePathA(a->fg,out);
+    treatA(a->elmt,out);
     free(a);
-    parcoursInfixeA(a->fd,out);
+    infixePathA(a->fd,out);
 }
 }
 
-void parcoursInfixeRA(PAVLAA a, FILE* out) {
+void infixePathRA(PAVLAA a, FILE* out) {
 if (a!=NULL) {
-    parcoursInfixeRA(a->fd,out); 
-    traiterA(a->elmt,out);
+    infixePathRA(a->fd,out); 
+    treatA(a->elmt,out);
     free(a);
-    parcoursInfixeRA(a->fg,out);
+    infixePathRA(a->fg,out);
 }
 }
 
-PAVLAA creerArbreA(DataA1 e){
-    PAVLAA noeud ;
-    noeud=malloc(sizeof(AVLAA));
-    if(noeud==NULL){
+PAVLAA createTreeA(DataA1 e){
+    PAVLAA tree ;
+    tree=malloc(sizeof(AVLAA));
+    if(tree==NULL){
         exit(1);
     }
-    noeud->elmt=e;
-    noeud->fg= NULL;
-    noeud->fd= NULL;
-    noeud->equilibre= 0;
-    noeud->elmt.average_increment=1;  
-    return noeud;
+    tree->elmt=e;
+    tree->fg= NULL;
+    tree->fd= NULL;
+    tree->balance= 0;
+    tree->elmt.average_increment=1;  
+    return tree;
 }
 
-PAVLAA rotationGaucheA(PAVLAA a){
+PAVLAA LeftRotationA(PAVLAA a){
     PAVLAA pivot; 
     float eq_a, eq_p;
 
     pivot = a->fd;
     a->fd = pivot->fg;
     pivot->fg = a;
-    eq_a = a->equilibre;
-    eq_p = pivot->equilibre;
-    a->equilibre = eq_a - max(eq_p, 0) - 1;
-    pivot->equilibre = min(min( eq_a-2, eq_a+eq_p-2), eq_p-1 );
+    eq_a = a->balance;
+    eq_p = pivot->balance;
+    a->balance = eq_a - max(eq_p, 0) - 1;
+    pivot->balance = min(min( eq_a-2, eq_a+eq_p-2), eq_p-1 );
     a = pivot;
     return a;
     }
 
-    PAVLAA rotationDroiteA(PAVLAA a){
+    PAVLAA RightRotationA(PAVLAA a){
     PAVLAA pivot ;
     float eq_a, eq_p;
 
     pivot = a->fg;
     a->fg = pivot->fd;
     pivot->fd = a;
-    eq_a = a->equilibre;
-    eq_p = pivot->equilibre;
-    a->equilibre = eq_a - min(eq_p, 0) + 1;
-    pivot->equilibre = max(max( eq_a+2, eq_a+eq_p+2), eq_p+1 );
+    eq_a = a->balance;
+    eq_p = pivot->balance;
+    a->balance = eq_a - min(eq_p, 0) + 1;
+    pivot->balance = max(max( eq_a+2, eq_a+eq_p+2), eq_p+1 );
     a = pivot;
     return a;
 }
 
-PAVLAA doubleRotationGaucheA(PAVLAA a){
-    a->fd = rotationDroiteA(a->fd);
-     return rotationGaucheA(a);
+PAVLAA doubleLeftRotationA(PAVLAA a){
+    a->fd = RightRotationA(a->fd);
+     return LeftRotationA(a);
 }
 
-PAVLAA doubleRotationDroiteA(PAVLAA a){
-    a->fg = rotationGaucheA(a->fg);
-     return rotationDroiteA(a);
+PAVLAA doubleRightRotationA(PAVLAA a){
+    a->fg = LeftRotationA(a->fg);
+     return RightRotationA(a);
 }
 
-PAVLAA equilibrerAVLAA(PAVLAA a){
+PAVLAA balanceAVLAA(PAVLAA a){
 
-    if (a->equilibre >=  2){
-        if (a->fd->equilibre >= 0){
-            return rotationGaucheA(a);
+    if (a->balance >=  2){
+        if (a->fd->balance >= 0){
+            return LeftRotationA(a);
         }
 
-        else return doubleRotationGaucheA(a);
+        else return doubleLeftRotationA(a);
     }
-    else if (a->equilibre  <=  -2){ 
-        if (a->fg->equilibre <= 0){
-            return rotationDroiteA(a);
+    else if (a->balance  <=  -2){ 
+        if (a->fg->balance <= 0){
+            return RightRotationA(a);
         }
 
     
 
-        else return doubleRotationDroiteA(a);
+        else return doubleRightRotationA(a);
     }
     return a;
 }
@@ -105,7 +105,7 @@ PAVLAA insertionAVLAAAverage(PAVLAA a,DataA1 e, int* h){
 
     if (a== NULL){
         *h=1;
-        return creerArbreA(e);
+        return createTreeA(e);
     }
     else if (e.station < a->elmt.station){
     a->fg=insertionAVLAAAverage(a->fg, e, h);
@@ -123,9 +123,9 @@ PAVLAA insertionAVLAAAverage(PAVLAA a,DataA1 e, int* h){
         return a;
     }
     if (*h != 0){
-        a->equilibre = a->equilibre + *h;
-        a=equilibrerAVLAA(a);
-        if (a->equilibre== 0){
+        a->balance = a->balance + *h;
+        a=balanceAVLAA(a);
+        if (a->balance== 0){
             *h = 0;
         }
         else *h = 1;
