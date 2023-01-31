8e363f28e583701a9cdcc6cf114adb3df1e24301
matiaskis <104195775+matiaskis@users.noreply.github.com>
2023-01-31 19:13:06 +0100

    Update avl1.h

--- avl1.h
+++ avl1.h
@@ -14,24 +14,124 @@ typedef struct avl1{
     Data1 elmt;
     struct avl1 * fg;
     struct avl1 * fd;
-    int equilibre;
+    int balance;
 }AVL1;
 
 typedef AVL1* PAVL1;
-void traiter(Data1 e,FILE* out);
-void parcoursInfixe(PAVL1 a, FILE* out);
-void parcoursInfixeR(PAVL1 a, FILE* out);
-int max(int a, int b);
-int min(int a, int b);
-PAVL1 creerArbre(Data1 e);
 
-PAVL1 rotationGauche(PAVL1 a);
-PAVL1 rotationDroite(PAVL1 a);
-PAVL1 doublerotationGauche(PAVL1 a);
-PAVL1 doublerotationDroite(PAVL1 a);
-PAVL1 equilibrerAVL1(PAVL1 a);
+/*!
+     * @brief                       this function is used to write all values needed in the output file                                            
+     * @param  e                    current element written
+     * @param  out                  pointer relative to ouput file
+     * @return                      nothing
+     */
+void traiter(Data1 e,FILE* out);
+
+/*!
+     * @brief                       this function is used to browse into  the tree                                            
+     * @param  a                    pointer relative to the tree
+     * @param  out                  pointer relative to ouput file
+     * @return                      nothing
+     */
+void parcoursInfixe(PAVL1 a, FILE* out);
+
+ /*!
+     * @brief                       this function is used to browse into  the tree but in reverse                                           
+     * @param  a                    pointer relative to the tree
+     * @param  out                  pointer relative to ouput file
+     * @return                      nothing
+     */
+void parcoursInfixeR(PAVL1 a, FILE* out);
+
+ /*!
+     * @brief                       this function is used to return the max of two integer value                                          
+     * @param  a                    pointer relative to the tree
+     * @param  out                  pointer relative to ouput file
+     * @return                      integer value
+     */
+int max(int a, int b);
+
+ /*!
+     * @brief                       this function is used to return the min of two integer value                                           
+     * @param  a                    pointer relative to the tree
+     * @param  out                  pointer relative to ouput file
+     * @return                      integer value
+     */
+int min(int a, int b);
+
+/*!
+     * @brief                       this function is used to create a new tree                                         
+     * @param  e                    element you want to insert first in the tree
+     * @return                      pointer relative to the tree
+     */
+PAVL1 createTree(Data1 e);
+
+/*!
+     * @brief                       this function is used to balance the tree                                         
+     * @param  a                    pointer relative to the tree
+     * @return                      pointer relative to the tree
+     */
+PAVL1 leftRotation(PAVL1 a);
+
+/*!
+     * @brief                       this function is used to balance the tree                                         
+     * @param  a                    pointer relative to the tree
+     * @return                      pointer relative to the tree
+     */
+PAVL1 rightRotation(PAVL1 a);
+
+/*!
+     * @brief                       this function is used to balance the tree                                         
+     * @param  a                    pointer relative to the tree
+     * @return                      pointer relative to the tree
+     */
+PAVL1 doubleLeftRotation(PAVL1 a);
+
+/*!
+     * @brief                       this function is used to balance the tree                                         
+     * @param  a                    pointer relative to the tree
+     * @return                      pointer relative to the tree
+     */
+PAVL1 doubleRightRotation(PAVL1 a);
+
+/*!
+     * @brief                       this function is used to balance the tree                                         
+     * @param  a                    pointer relative to the tree
+     * @return                      pointer relative to the tree
+     */
+PAVL1 balanceAVL1(PAVL1 a);
+
+/*!
+     * @brief                       this function is used to insert an element in the tree, keeping the max value of associated_data                                         
+     * @param  a                    pointer relative to the tree
+     * @param  e                    element to insert
+     * @param h                     pointer relative to an integer value used to calculate the balance within th tree
+     * @return                      pointer relative to the tree
+     */
 PAVL1 insertionAVL1Max(PAVL1 a,Data1 e, int* h);
+
+/*!
+     * @brief                       this function is used to insert an element in the tree, keeping the min value of associated_data                                         
+     * @param  a                    pointer relative to the tree
+     * @param  e                    element to insert
+     * @param h                     pointer relative to an integer value used to calculate the balance within th tree
+     * @return                      pointer relative to the tree
+     */
 PAVL1 insertionAVL1Min(PAVL1 a,Data1 e, int* h);
+
+/*!
+     * @brief                       this function is used to insert an element in the tree,keeping the the gross value of all associated_data                                       
+     * @param  a                    pointer relative to the tree
+     * @param  e                    element to insert
+     * @param h                     pointer relative to an integer value used to calculate the balance within th tree
+     * @return                      pointer relative to the tree
+     */
 PAVL1 insertionAVL1Average(PAVL1 a,Data1 e, int* h);
+
+/*!
+     * @brief                       this function is used to calculate the average of the associated data in each element of the tree                                         
+     * @param  a                    pointer relative to the tree
+     * @return                      nothing
+     */
 void calculateAverage(PAVL1 a);
 #endif
