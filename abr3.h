9ffe799293427d1cad5907cec288ac4fa835c5c6
matiaskis <104195775+matiaskis@users.noreply.github.com>
2023-02-01 00:08:55 +0100

    Update abr3.h

--- abr3.h
+++ abr3.h
@@ -80,7 +80,7 @@ typedef ABR3* PABR3;
      * @param  e                    element to insert
      * @return                      pointer relative to the tree
      */
-    PABR3 insertionABR3SecondTree(PABR3 a,Data3 e);
+    PABR3 insertionABR3SecondTree(PABR3 a,Data_abr3 e);
     
     /*!
      * @brief                       this function is used to insert an element in the first tree                                      
@@ -88,5 +88,5 @@ typedef ABR3* PABR3;
      * @param  e                    element to insert
      * @return                      pointer relative to the tree
      */
-    PABR3 insertionABR_oabr3(PABR3 a,Data3 e);
+    PABR3 insertionABR_oabr3(PABR3 a,Data_abr3 e);
 #endif
