413e047bf3f4da4d68294c0b6450c10ae1b3a89c
matiaskis <104195775+matiaskis@users.noreply.github.com>
2023-01-31 17:22:52 +0100

    Update abr1.h

--- abr1.h
+++ abr1.h
@@ -19,12 +19,65 @@ typedef struct abr1{
 
 typedef ABR1* PABR1;
 
-void treat_abr1(Data1ABR e,FILE* out) ;
-void infixPath_abr1(PABR1 a, FILE* out);
-void infixPathR_abr1(PABR1 a, FILE* out);
-PABR1 createTree_abr1(Data1ABR e);
-PABR1 insertionMax_abr1(PABR1 a,Data1ABR e);
-PABR1 insertionMin_abr1(PABR1 a,Data1ABR e);
-PABR1 insertionAverage_abr1(PABR1 a, Data1ABR e);
-void calculateAverage_abr1(PABR1 a);
+    /*!
+     * @brief                       this function is used to write all values needed in the output file                                            
+     * @param  e                    current element written
+     * @param  out                  pointer relative to ouput file
+     * @return                      nothing
+     */
+     void treat_abr1(Data1ABR e,FILE* out) ;
+
+    /*!
+     * @brief                       this function is used to browse into  the tree                                            
+     * @param  a                    pointer relative to the tree
+     * @param  out                  pointer relative to ouput file
+     * @return                      nothing
+     */
+    void infixPath_abr1(PABR1 a, FILE* out);
+
+    /*!
+     * @brief                       this function is used to browse into  the tree but in reverse                                           
+     * @param  a                    pointer relative to the tree
+     * @param  out                  pointer relative to ouput file
+     * @return                      nothing
+     */
+     void infixPathR_abr1(PABR1 a, FILE* out);
+
+    /*!
+     * @brief                       this function is used to create a new tree                                         
+     * @param  e                    element you want to insert first in the tree
+     * @return                      pointer relative to the tree
+     */
+    PABR1 createTree_abr1(Data1ABR e);
+
+    /*!
+     * @brief                       this function is used to insert a element in the tree, keeping the max value of associated_data                                         
+     * @param  a                    pointer relative to the tree
+     * @param  e                    element to insert
+     * @return                      pointer relative to the tree
+     */
+    PABR1 insertionMax_abr1(PABR1 a,Data1ABR e);
+
+    /*!
+     * @brief                       this function is used to insert a element in the tree, keeping the min value of associated_data                                          
+     * @param  a                    pointer relative to the tree
+     * @param  e                    element to insert
+     * @return                      pointer relative to the tree
+     */
+    PABR1 insertionMin_abr1(PABR1 a,Data1ABR e);
+
+    /*!
+     * @brief                       this function is used to insert a element in the tree, keeping the the gross value of all associated_data                                          
+     * @param  a                    pointer relative to the tree
+     * @param  e                    element to insert
+     * @return                      pointer relative to the tree
+     */
+    PABR1 insertionAverage_abr1(PABR1 a, Data1ABR e);
+
+    /*!
+     * @brief                       this function is used to calculate the average of the associated data in each element of the tree                                         
+     * @param  a                    pointer relative to the tree
+     * @return                      nothing
+     */
+    void calculateAverage_abr1(PABR1 a);
 #endif
