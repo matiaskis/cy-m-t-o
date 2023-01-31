92e4e7d06c41ed2bba36262511de89270c46c0d0
matiaskis <104195775+matiaskis@users.noreply.github.com>
2023-01-31 19:28:49 +0100

    Update avl2.h

--- avl2.h
+++ avl2.h
@@ -14,22 +14,90 @@ typedef struct avl2{
     Data2 elmt;
     struct avl2 * fg;
     struct avl2 * fd;
-    int equilibre;
+    int balance;
 }AVL2;
 
 typedef AVL2* PAVL2;
 
-void traiter2(Data2 e,FILE* out);
-void parcoursInfixe2(PAVL2 a, FILE* out);
-void parcoursInfixeR2(PAVL2 a, FILE* out);
+/*!
+     * @brief                       this function is used to write all values needed in the output file                                            
+     * @param  e                    current element written
+     * @param  out                  pointer relative to ouput file
+     * @return                      nothing
+     */
+void treat2(Data2 e,FILE* out);
 
-PAVL2 creerArbre2(Data2 e);
+/*!
+     * @brief                       this function is used to browse into  the tree                                            
+     * @param  a                    pointer relative to the tree
+     * @param  out                  pointer relative to ouput file
+     * @return                      nothing
+     */
+void infixPath2(PAVL2 a, FILE* out);
 
-PAVL2 rotationGauche2(PAVL2 a);
-PAVL2 rotationDroite2(PAVL2 a);
-PAVL2 doublerotationGauche2(PAVL2 a);
-PAVL2 doublerotationDroite2(PAVL2 a);
-PAVL2 equilibrerAVL2(PAVL2 a);
+ /*!
+     * @brief                       this function is used to browse into  the tree but in reverse                                           
+     * @param  a                    pointer relative to the tree
+     * @param  out                  pointer relative to ouput file
+     * @return                      nothing
+     */
+void infixPathR2(PAVL2 a, FILE* out);
+
+/*!
+     * @brief                       this function is used to create a new tree                                         
+     * @param  e                    element you want to insert first in the tree
+     * @return                      pointer relative to the tree
+     */
+PAVL2 createTree2(Data2 e);
+
+/*!
+     * @brief                       this function is used to balance the tree                                         
+     * @param  a                    pointer relative to the tree
+     * @return                      pointer relative to the tree
+     */
+PAVL2 LeftRotation2(PAVL2 a);
+
+/*!
+     * @brief                       this function is used to balance the tree                                         
+     * @param  a                    pointer relative to the tree
+     * @return                      pointer relative to the tree
+     */
+PAVL2 RightRotation2(PAVL2 a);
+
+/*!
+     * @brief                       this function is used to balance the tree                                         
+     * @param  a                    pointer relative to the tree
+     * @return                      pointer relative to the tree
+     */
+PAVL2 doubleLeftRotation2(PAVL2 a);
+
+/*!
+     * @brief                       this function is used to balance the tree                                         
+     * @param  a                    pointer relative to the tree
+     * @return                      pointer relative to the tree
+     */
+PAVL2 doubleRightRotation2(PAVL2 a);
+
+/*!
+     * @brief                       this function is used to balance the tree                                         
+     * @param  a                    pointer relative to the tree
+     * @return                      pointer relative to the tree
+     */
+PAVL2 balanceAVL2(PAVL2 a);
+
+/*!
+     * @brief                       this function is used to insert an element in the tree,keeping the the gross value of all associated_data                                       
+     * @param  a                    pointer relative to the tree
+     * @param  e                    element to insert
+     * @param h                     pointer relative to an integer value used to calculate the balance within th tree
+     * @return                      pointer relative to the tree
+     */
 PAVL2 insertionAVLAverage2(PAVL2 a,Data2 e, int* h);
+
+/*!
+     * @brief                       this function is used to calculate the average of the associated data in each element of the tree                                         
+     * @param  a                    pointer relative to the tree
+     * @return                      nothing
+     */
 void calculateAverage2(PAVL2 a);
 #endif
