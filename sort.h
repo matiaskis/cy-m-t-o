5eb61b3c76e2b8766d0361c99477265cb93e5730
khorz <104204070+khorz@users.noreply.github.com>
2023-02-03 00:36:15 +0100

    Update sort.h

--- sort.h
+++ sort.h
@@ -6,6 +6,6 @@
 
 void sort_abr(int sort_option,int display_option,int sort_option2,File* data_file,File* output_file);
 void sort_avl(int sort_option,int display_option,int sort_option2,File* data_file,File* output_file);
-sort_tab(int sort_option,int display_option,int sort_option2,File* data_file,File* output_file);
+void sort_tab(int sort_option,int display_option,int sort_option2,File* data_file,File* output_file);
 
 #endif
