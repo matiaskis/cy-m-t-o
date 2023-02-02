e5d4f90091f181b4e739da1f2cb58e33e887762b
khorz <104204070+khorz@users.noreply.github.com>
2023-02-03 00:35:41 +0100

    Add files via upload

---
+++ sort.c
@@ -0,0 +1,394 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "avl1.h"
+#include "avlh.h"
+#include "avl2.h"
+#include "avl3.h"
+#include "avlM.h"
+#include "avlA.h"
+#include "abr1.h"
+#include "abrh.h"
+#include "abr2.h"
+#include "abr3.h"
+#include "abrM.h"
+#include "abrA.h"
+#include "sort.h"
+void sort_tab(int sort_option,int display_option,int sort_option2,File* data_file,File* output_file){
+    printf("pouloulou")
+}
+void sort_abr(int sort_option,int display_option,int sort_option2,File* data_file,File* output_file){
+
+    int station; 
+    float associated_data,associated_data2; 
+    int height, moisture; 
+    long date; 
+    float latitude, longitude;
+    //sort P1 T1
+    if(sort_option==1){
+        PABR1 a1=NULL;
+        Data1ABR data1;
+
+        while (fscanf(data_file,"%d;",&station) == 1){
+            fscanf(data_file,"%f",&associated_data);
+            data1.station=station;
+            data1.associated_Data1=associated_data; 
+            a1=insertionMax_abr1(a1,data1);
+        }
+        if(display_option==1){
+        infixPath_abr1(a1,output_file);
+        }
+        else infixPathR_abr1(a1,output_file);
+    }
+
+    //sort P1 T1
+    else if(sort_option==2){
+        PABR1 a1=NULL;
+        Data1ABR data1;
+
+        while (fscanf(data_file,"%d;",&station) == 1){
+            fscanf(data_file,"%f",&associated_data);
+            data1.station=station;
+            data1.associated_Data1=associated_data;
+            a1=insertionMin_abr1(a1,data1);
+        }
+        if(display_option==1){
+            infixPath_abr1(a1,output_file);
+        }
+        else infixPathR_abr1(a1,output_file);
+    }
+
+     //sort P1 T1
+    else if(sort_option==3){
+        PABR1 a1=NULL;
+        Data1ABR data1;
+
+        while (fscanf(data_file,"%d;",&station) == 1){
+            fscanf(data_file,"%f",&associated_data);
+            data1.station=station;
+            data1.associated_Data1=associated_data;
+            a1=insertionAverage_abr1(a1,data1);
+        }
+        calculateAverage_abr1(a1);
+        if(display_option==1){
+            infixPath_abr1(a1,output_file);
+        }
+        else infixPathR_abr1(a1,output_file);
+    }
+
+    //sort height
+    else if(sort_option==4){
+        PABR_H ah=NULL;
+        Data_abrh data_h;
+
+        while(fscanf(data_file,"%d;",&station)==1){
+            fscanf(data_file,"%f,",&latitude);
+            fscanf(data_file,"%f;",&longitude);
+            fscanf(data_file,"%d",&height);
+            data_h.station=station;
+            data_h.height=height;
+            data_h.latitude=latitude;
+            data_h.longitude=longitude;
+            ah=insertionABRHeight(ah,data_h);
+        }
+        if(display_option==1){
+            infixPath_abrh(ah,output_file);
+        }
+        else infixPathR_abrh(ah,output_file);
+    }
+
+    //sort P2 T2
+    else if(sort_option==5){
+        PABR2 a2=NULL;
+        Data_abr2 data2;
+
+         while (fscanf(data_file,"%ld;",&date) == 1){
+            fscanf(data_file,"%f",&associated_data);
+            data2.date2=date;
+            data2.associated_Data2=associated_data;
+            a2=insertionAverage_abr2(a2,data2);
+        }
+        calculateAverage_abr2(a2);
+        if(display_option==1){
+            infixPath_abr2(a2,output_file);
+        }
+        else infixPathR_abr2(a2,output_file);
+    }
+
+    //sort P3 T3
+    else if(sort_option==6){
+         PABR3 a3=NULL;
+        Data_abr3 data3;
+
+         while (fscanf(data_file,"%d;",&station ) == 1){
+            fscanf(data_file,"%ld;",&date);
+            fscanf(data_file,"%f",&associated_data);
+            data3.date3=date;
+            data3.station=station;
+            data3.associated_Data3=associated_data;
+            a3=insertionABR_oabr3(a3,data3);
+        }
+
+        if(display_option==1){
+            infixPath_abr3(a3,output_file);
+        }
+        else infixPathR_abr3(a3,output_file);
+    }
+
+    //sort moisture
+    else if(sort_option==7){      
+        PABR_m am=NULL;
+        Data_abrm data_m;
+
+        if(sort_option2 == 1){
+            while (fscanf(data_file,"%d;",&station) == 1){
+                fscanf(data_file,"%d;",&moisture);
+                fscanf(data_file,"%f,",&latitude);
+                fscanf(data_file,"%f",&longitude);
+                data_m.station=station;
+                data_m.moisture=moisture;
+                data_m.latitude=latitude;
+                data_m.longitude=longitude;
+                am=insertionABRMMax(am,data_m);
+            }
+            if(display_option==1){
+                infixPath_abrm2(am,output_file);
+            }
+            else infixPathR_abrm2(am,output_file);
+        }
+        else{
+            while (fscanf(data_file,"%d;",&station) == 1){
+                fscanf(data_file,"%d;",&moisture);
+                fscanf(data_file,"%f,",&latitude);
+                fscanf(data_file,"%f",&longitude);
+                data_m.station=station;
+                data_m.moisture=moisture;
+                data_m.latitude=latitude;
+                data_m.longitude=longitude;
+                am=insertionABRM(am,data_m);
+
+            }
+
+            if(display_option==1){
+                infixPath_abrm(am,output_file);
+            }
+            else infixPathR_abrm(am,output_file);
+        }
+
+        
+       
+    }
+
+    //sort wind
+    else if(sort_option==8){
+         PABRAA aa=NULL;
+        Data_abra dataA1;
+
+        while (fscanf(data_file,"%d;",&station) == 1){
+            fscanf(data_file,"%f;",&associated_data);
+            fscanf(data_file,"%f;",&associated_data2);
+            fscanf(data_file,"%f,",&latitude);
+            fscanf(data_file,"%f",&longitude);
+            dataA1.station=station;
+            dataA1.associated_DataA1=associated_data;
+            dataA1.associated_DataA2=associated_data2;
+            dataA1.latitude=latitude;
+            dataA1.longitude=longitude;
+            aa=insertionAverage_abra(aa,dataA1);
+        }
+        calculateAverage_abra(aa);
+        if(display_option==1){
+            infixPathA_abra(aa,output_file);
+        }
+        else infixPathRA_abra(aa,output_file);
+    }  
+
+
+}
+void sort_avl(int sort_option,int display_option,int sort_option2,File* data_file,File* output_file){
+
+    int h;
+    int station; 
+    float associated_data,associated_data2; 
+    int height, moisture; 
+    long date; 
+    int g;
+    float latitude, longitude;
+
+    //sort P1 T1
+    if(sort_option==1){
+        PAVL1 a1=NULL;
+        Data1 data1;
+
+        while (fscanf(data_file,"%d;",&station) == 1){
+            fscanf(data_file,"%f",&associated_data);
+            data1.station=station;
+            data1.associated_Data1=associated_data; 
+            a1=insertionAVL1Max(a1,data1,&h);
+        }
+        if(display_option==1){
+        infixPath(a1,output_file);
+        }
+        else infixPathR(a1,output_file);
+    }
+
+    //sort P1 T1
+    else if(sort_option==2){
+        PAVL1 a1=NULL;
+        Data1 data1;
+
+        while (fscanf(data_file,"%d;",&station) == 1){
+            fscanf(data_file,"%f",&associated_data);
+            data1.station=station;
+            data1.associated_Data1=associated_data;
+            a1=insertionAVL1Min(a1,data1,&h);
+        }
+        if(display_option==1){
+            infixPath(a1,output_file);
+        }
+        else infixPathR(a1,output_file);
+        }
+
+    //sort P1 T1
+    else if(sort_option==3){
+        PAVL1 a1=NULL;
+        Data1 data1;
+
+        while (fscanf(data_file,"%d;",&station) == 1){
+            fscanf(data_file,"%f",&associated_data);
+            data1.station=station;
+            data1.associated_Data1=associated_data;
+            a1=insertionAVL1Average(a1,data1,&h);
+        }
+        calculateAverage(a1);
+        if(display_option==1){
+            infixPath(a1,output_file);
+        }
+        else infixPathR(a1,output_file);
+    }
+
+    //sort height
+    else if(sort_option==4){
+        PAVL_H ah=NULL;
+        Data_h data_h;
+
+        while(fscanf(data_file,"%d;",&station)==1){
+            fscanf(data_file,"%f,",&latitude);
+            fscanf(data_file,"%f;",&longitude);
+            fscanf(data_file,"%d",&height);
+            data_h.station=station;
+            data_h.height=height;
+            data_h.latitude=latitude;
+            data_h.longitude=longitude;
+            ah=insertionAVLHeight(ah,data_h,&h);
+        }
+        if(display_option==1){
+            infixPath_h(ah,output_file);
+        }
+        else infixPathR_h(ah,output_file);
+    }
+
+    //sort P2 T2
+    else if(sort_option==5){
+        PAVL2 a2=NULL;
+        Data2 data2;
+
+         while (fscanf(data_file,"%ld;",&date) == 1){
+            fscanf(data_file,"%f",&associated_data);
+            data2.date2=date;
+            data2.associated_Data2=associated_data;
+            a2=insertionAVLAverage2(a2,data2,&h);
+        }
+        calculateAverage2(a2);
+        if(display_option==1){
+            infixPath2(a2,output_file);
+        }
+        else infixPathR2(a2,output_file);
+    }
+
+    //sort P3 T3
+    else if(sort_option==6){
+        PAVL3 a3=NULL;
+        Data3 data3;
+
+         while (fscanf(data_file,"%d;",&station ) == 1){
+            fscanf(data_file,"%ld;",&date);
+            fscanf(data_file,"%f",&associated_data);
+            data3.date3=date;
+            data3.station=station;
+            data3.associated_Data3=associated_data;
+            a3=insertionAVL3date(a3,data3,&h,&g);
+        }
+
+        if(display_option==1){
+            infixPath3(a3,output_file);
+        }
+        else infixPathR3(a3,output_file);
+    }
+
+    //sort moisture
+    else if(sort_option==7){      
+        PAVL_m am=NULL;
+        Data_m data_m;
+
+        if(sort_option2 == 1){
+            while (fscanf(data_file,"%d;",&station) == 1){
+                fscanf(data_file,"%d;",&moisture);
+                fscanf(data_file,"%f,",&latitude);
+                fscanf(data_file,"%f",&longitude);
+                data_m.station=station;
+                data_m.moisture=moisture;
+                data_m.latitude=latitude;
+                data_m.longitude=longitude;
+                am=insertionAVLMMax(am,data_m,&h);
+            }
+            if(display_option==1){
+                infixPath_m2(am,output_file);
+            }
+            else infixPathR_m2(am,output_file);
+        }
+        else{
+            while (fscanf(data_file,"%d;",&station) == 1){
+                fscanf(data_file,"%d;",&moisture);
+                fscanf(data_file,"%f,",&latitude);
+                fscanf(data_file,"%f",&longitude);
+                data_m.station=station;
+                data_m.moisture=moisture;
+                data_m.latitude=latitude;
+                data_m.longitude=longitude;
+                am=insertionAVLM(am,data_m,&h);
+
+            }
+
+            if(display_option==1){
+                infixPath_m(am,output_file);
+            }
+            else infixPathR_m(am,output_file);
+        }              
+    }
+
+    //sort wind
+    else if(sort_option==8){
+        PAVLAA aa=NULL;
+        DataA1 dataA1;
+
+        while (fscanf(data_file,"%d;",&station) == 1){
+            fscanf(data_file,"%f;",&associated_data);
+            fscanf(data_file,"%f;",&associated_data2);
+            fscanf(data_file,"%f,",&latitude);
+            fscanf(data_file,"%f",&longitude);
+            dataA1.station=station;
+            dataA1.associated_DataA1=associated_data;
+            dataA1.associated_DataA2=associated_data2;
+            dataA1.latitude=latitude;
+            dataA1.longitude=longitude;
+            aa=insertionAVLAAAverage(aa,dataA1,&h);
+        }
+        calculateAverageA(aa);
+        if(display_option==1){
+            infixPathA(aa,output_file);
+        }
+        else infixPathRA(aa,output_file);
+    }  
+
+}
---
+++ sort.h
@@ -0,0 +1,11 @@
+#ifndef SORT_H
+#define SORT_H
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+void sort_abr(int sort_option,int display_option,int sort_option2,File* data_file,File* output_file);
+void sort_avl(int sort_option,int display_option,int sort_option2,File* data_file,File* output_file);
+sort_tab(int sort_option,int display_option,int sort_option2,File* data_file,File* output_file);
+
+#endif
