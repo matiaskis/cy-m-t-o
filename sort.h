19f639eb6f59ad40c03ccacb6c5457d830eb1b7c
matiaskis <104195775+matiaskis@users.noreply.github.com>
2023-02-03 01:14:50 +0100

    Update sort.h

--- sort.h
+++ sort.h
@@ -4,8 +4,9 @@
 #include <stdlib.h>
 #include <string.h>
 
-void sort_abr(int sort_option,int display_option,int sort_option2,File* data_file,File* output_file);
-void sort_avl(int sort_option,int display_option,int sort_option2,File* data_file,File* output_file);
-void sort_tab(int sort_option,int display_option,int sort_option2,File* data_file,File* output_file);
+void sort_abr(int sort_option,int display_option,int sort_option2,FILE* data_file,FILE* output_file);
+void sort_avl(int sort_option,int display_option,int sort_option2,FILE* data_file,FILE* output_file);
+void sort_tab(int sort_option,int display_option,int sort_option2,FILE* data_file,FILE* output_file);
 
 #endif
+
